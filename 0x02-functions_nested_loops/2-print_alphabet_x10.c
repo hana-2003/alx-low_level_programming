b318152b72e84739e6f67339f1fba36278600209
hana <hanamahmoudsalah@gmail.com>
2023-07-18 17:25:50 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,3 @@
-
 #include "main.h"
 
 /**
@@ -6,20 +5,21 @@
  * followed by a new line
  */
 void print_alphabet_x10(void)
-{	
-char ch;
-int i;
-i = 0;
-while (i < 10)
 {
-ch = 'a';
-while (ch <= 'z')
-{
-_putchar(ch);
-ch++;
-}
-_putchar('\n');
-i++;}
-}
+	char ch;
+	int i;
 
+	i = 0;
 
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
