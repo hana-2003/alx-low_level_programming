38138648b1c8c807ce78b2dd52db4b1be3f14380
hana <hanamahmoudsalah@gmail.com>
2023-08-05 16:45:53 -0700

    a

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,26 +1,27 @@
 #include "main.h"
-
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination string.
- * @src: The source string.
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
  *
- * Return: A pointer to the resulting string dest.
+ * Return: void
  */
 char *_strcat(char *dest, char *src)
 {
-int dest_len = 0, i;
-
-/* Find the length of dest string */
-while (dest[dest_len] != '\0')
-dest_len++;
-
-/* Append the src string to dest */
-for (i = 0; src[i] != '\0'; i++)
-dest[dest_len + i] = src[i];
-
-/* Add null terminator to dest */
-dest[dest_len + i] = '\0';
-
-return dest;
+	int i;
+	int j;
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,30 @@
-#include"main.h"
-
+#include "main.h"
 /**
-*_strncat-concatenatetwostrings
-*usingatmostnbytesfromsrc
-*@dest:inputvalue
-*@src:inputvalue
-*@n:inputvalue
-*Return:dest
-*/
-char*_strncat(char*dest,char*src,intn)
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
 {
-inti;
-intj;
-i=0;
-while(dest[i]!='\0')
-{
-i++;
-}
-j=0;
-while(j<n&&src[j]!='\0')
-{
-dest[i]=src[j];
-i++;
-j++;
-}
-dest[i]='\0';
-return(dest);
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+	dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
